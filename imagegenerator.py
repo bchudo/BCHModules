c09ac0252516548c4c403e6bf877e4b38ec8b1ca
bchudo <151437191+bchudo@users.noreply.github.com>
2023-11-26 15:50:28 +0500

    Add files via upload

---
+++ imagegenerator.py
@@ -0,0 +1,97 @@
+# meta developer: @bchmodules
+# meta pic: https://0x0.st/HwtW.jpg
+# meta banner: https://0x0.st/HwEW.jpg
+
+from .. import loader, utils
+
+import random
+from contextlib import suppress
+from telethon.tl.types import Message
+import asyncio
+
+@loader.tds
+class ImageGeneratorMod(loader.Module):
+	""""""
+	strings = {
+		"name": "ImageGenerator",
+		"chatgpt_disabled": "❌ <b>ChatGPT in prompt generations disabled</b>",
+		"chatgpt_enabled": "✅ <b>ChatGPT in prompt generations enabled</b>",
+		"no_args": "❌ No arguments!",
+		"generation": "<b>⏳ Generation . . .</b>\n\n<b>Prompt</b>: <i>{}</i>"
+	}
+	strings_ru = {
+		"name": "ImageGenerator",
+		"chatgpt_disabled": "❌ <b>ChatGPT в генерациях запроса выключен</b>",
+		"chatgpt_enabled": "✅ <b>ChatGPT в генерациях запроса включен</b>",
+		"no_args": "❌ Нет аргументов!",
+		"generation": "<b>⏳ Генерация . . .</b>\n\n<b>Промпт</b>: <i>{}</i>"
+	}
+
+	def __init__(self):
+		self.use_chatgpt = False
+
+	async def client_ready(self):
+		await self.request_join(
+			"@bchmodules",
+			"Подпишись на канал, дружище :3"
+		)
+		check = await self.client.get_messages("@YamiChat_bot", limit=1)
+		try:
+			check[0]
+		except IndexError:
+			await self.client.send_message("@YamiChat_bot", "/start")
+		await self.request_join(
+			"@YamiChannel",
+			"Подпишись, иначе модуль не будет работать"
+		)
+
+	@loader.command(ru_doc="использовать ChatGPT генерациях запроса? По умолчанию - нет.")
+	async def usechatgptcmd(self, message: Message):
+		"""Use ChatGPT in generation prompt? Default - no."""
+		if self.use_chatgpt:
+			self.use_chatgpt = False
+			await utils.answer(message=message, response=self.strings("chatgpt_disabled"))
+			return
+		self.use_chatgpt = True
+		await utils.answer(message=message, response=self.strings("chatgpt_enabled"))
+
+	@loader.command(ru_doc="<prompt> сгенерировать изображение")
+	async def imgcmd(self, message: Message):
+		"""<prompt> generate image"""
+		args = utils.get_args_raw(message)
+		if not args:
+			await utils.answer(message=message, response=self.strings("no_args"))
+			return
+		async with self.client.conversation("@YamiChat_bot") as conv:
+			await utils.answer(message=message, response=self.strings("generation").format(args))
+			prompt = ""
+			if self.use_chatgpt:
+				await conv.send_message(f"/ask дай мне промпт на английском языке, где {args}. без лишних \"да, конечно, вот твой промпт\", \"промпт:\" и т.д.")
+				prompt = (await conv.get_response()).text
+				lines = prompt.split("\n")
+				for i in lines:
+					if "Потрачено токенов" in i:
+						lines.remove(i)
+				prompt = "".join(lines)
+			else:
+				prompt = args
+			await conv.send_message(f"/img {prompt}")
+		while True:
+			await asyncio.sleep(3)
+			msgs = await self.client.get_messages("@YamiChat_bot", limit=1)
+			if "Image for: " in msgs[0].text:
+				await message.delete()
+				break
+		images_list = []
+		images = await self.client.get_messages("@YamiChat_bot", limit=4)
+		for i in images:
+			if i.media is not None:
+				images_list.append(i.media)
+		reply = await message.get_reply_message()
+		await self.client.send_file(
+			message.peer_id,
+			reply_to=reply.id if reply else None,
+			file=images_list,
+			caption= (images[0].text + "\n\nCreated by <b><a href=\"https://t.me/YamiChat_bot\">Yami</a></b>") if images[0].text else None,
+			force_document=False
+		)
